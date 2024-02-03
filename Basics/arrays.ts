2f0b54e8f74f77ebde757b35c46a6584e612aa97
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2024-02-03 19:30:19 +0530

    Add files via upload

---
+++ Basics/arrays.ts
@@ -0,0 +1,20 @@
+const heros:string[]=[];
+heros.push("Ironman");
+
+//Another way to define
+// const heros:Array<number>=[]
+
+const power:number[]=[];
+power.push(69)
+
+type user={
+    name:string,
+    email:string
+}
+
+const myUsers:user[]=[];
+myUsers.push({name:"paul",email:"abc@gmail.com"});
+
+const MLmodels:number[][]=[
+    [244,69.96],[11,22,33]
+]
