08b1112146521b629413d12b4a4245a9ad81126d
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2024-02-04 13:16:45 +0530

    Add files via upload

---
+++ Basics/arrays.js
@@ -0,0 +1,11 @@
+var heros = [];
+heros.push("Ironman");
+//Another way to define
+// const heros:Array<number>=[]
+var power = [];
+power.push(69);
+var myUsers = [];
+myUsers.push({ name: "paul", email: "abc@gmail.com" });
+var MLmodels = [
+    [244, 69.96], [11, 22, 33]
+];
