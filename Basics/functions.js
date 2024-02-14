51bae742adc7fafa9dcd836eb1d84b6f68b6e441
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2024-02-14 20:01:42 +0530

    Add files via upload

---
+++ Basics/functions.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUp(name, email, isPaid) { }
+var login = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+addTwo(5);
+getUpper("Paul");
+signUp("Paul", "abc@gmail.com", true);
+login("Paul", "abc@gmail.com");
