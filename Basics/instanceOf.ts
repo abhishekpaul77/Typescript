d68b7d701fef86a3b9b6e5424ca8734507c34d9a
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2024-02-15 12:55:38 +0530

    Add files via upload

---
+++ Basics/instanceOf.ts
@@ -0,0 +1,33 @@
+//Instance of
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+// Type predicates
+type Fish = {
+  swim: () => void;
+};
+
+type Bird = {
+  fly: () => void;
+};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "Fish food";
+  } else {
+    pet;
+    return "Bird foood";
+  }
+}
+
+
