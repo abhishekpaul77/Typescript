b60ca651501089a88b59bfa6cf48c4f7416d3c1a
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2024-02-16 20:01:21 +0530

    Add files via upload

---
+++ Basics/instanceOf.js
@@ -0,0 +1,22 @@
+//Instance of
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "Fish food";
+    }
+    else {
+        pet;
+        return "Bird foood";
+    }
+}
