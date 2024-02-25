c96ade786f4a69b40abcbfdadcef3ecf2a322125
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2024-02-25 20:09:16 +0530

    Add files via upload

---
+++ Basics/objects.js
@@ -0,0 +1,14 @@
+"use strict";
+// const user={
+//     name:"Paul",
+//     email:"abc@gamil.com",
+//     isActive:true
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+var myUser = {
+    _id: "",
+    name: "abc",
+    email: "abc@gmail.com",
+    isActive: true
+};
+myUser.name = "xyz";
