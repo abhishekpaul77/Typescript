91d1e1c3cbea67c2de387695f51381726ba1e313
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2024-02-27 09:26:08 +0530

    Add files via upload

---
+++ Basics/tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Array
+var user = [1, 2, 3, "Paul"]; //Here string & number can be in any order
+//Tuples
+var user2 = ["Paul", 69]; //Here string & number needs to be in a specific order and must follow a strict sequence
+var rgb = [255, 22, 21];
+var newUser = [123, "Abhishek"]; //Must follow User type in order
+newUser.push[1] = "call me"; //This is okay
